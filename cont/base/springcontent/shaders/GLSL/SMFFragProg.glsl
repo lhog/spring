e91c07d3c63db8b7a29525c1ec5ec75b78ccb0f8
LHoG <ccna@mail.ru>
2020-04-12 11:47:56 +0300

    Undo accident change

--- cont/base/springcontent/shaders/GLSL/SMFFragProg.glsl
+++ cont/base/springcontent/shaders/GLSL/SMFFragProg.glsl
@@ -47,10 +47,10 @@ uniform vec2 mapHeights; // min & max height on the map
 uniform vec4 lightDir;
 uniform vec3 cameraPos;
 
-in vec3 halfDir;
-in float fogFactor;
-in vec4 vertexWorldPos;
-in vec2 diffuseTexCoords;
+varying vec3 halfDir;
+varying float fogFactor;
+varying vec4 vertexWorldPos;
+varying vec2 diffuseTexCoords;
 
 #ifdef HAVE_INFOTEX
 	uniform sampler2D infoTex;
